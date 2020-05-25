89a18b61a8e75c67da5c14a922014f869ca54533
Diana Karen Parra Sosa <dianaparra092@gmail.com>
2020-05-25 16:50:47 -0500

    fusion del frontend y backend

---
+++ core/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+# Create your views here.
+
+
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def services(request):
+    return render(request, "core/services.html")
+
+def store(request):
+    return render(request, "core/store.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
+
+def blog(request):
+    return render(request, "core/home.html")
+
+def sample(request):
+    return render(request, "core/sample.html")

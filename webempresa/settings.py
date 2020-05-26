755d04574e4ba0b9fece444c0c4ac2da2b883ec6
Diana Karen Parra Sosa <dianaparra092@gmail.com>
2020-05-25 20:16:11 -0500

    ipersonalización del administrador

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
     'core',
     'services.apps.ServicesConfig',
+
 ]
 
 MIDDLEWARE = [
